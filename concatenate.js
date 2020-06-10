453fa1e2052e0cde4c3a2a4fc28efe84a657e1e7
Alex Noel <auzaka@gmail.com>
2020-06-10 11:30:41 +0300

    include translations/ folder as well.

--- concatenate.js
+++ concatenate.js
@@ -3,14 +3,17 @@ const tmp = require("tmp");
 const Promise = require("bluebird");
 const fs = require("fs");
 const vm = require("vm");
-const getTranslationsList = PATH => {
-  const array = shell.find(PATH || shell.pwd()).filter(file => {
-    return file.match(/translations\.js$/);
+const getTranslationsList = (PATH) => {
+  const array = shell.find(PATH || shell.pwd()).filter((filePath) => {
+    return (
+      filePath.match(/translations\.js$/) ||
+      (filePath.match(/\/translations\//) && !filePath.endsWith("index.js"))
+    );
   });
   return array;
 };
 
-const createTempFiles = filePathList => {
+const createTempFiles = (filePathList) => {
   const tmpFolder = tmp.dirSync();
   filePathList.forEach((filePath, key) => {
     const newPath = `${tmpFolder.name}/${key}-translation.js`;
@@ -21,9 +24,9 @@ const createTempFiles = filePathList => {
   return tmpFolder;
 };
 
-const preprocessJs = async tmpFolder => {
+const preprocessJs = async (tmpFolder) => {
   const newList = shell.find(`${tmpFolder.name}/*.js`);
-  await Promise.map(newList, filePath => {
+  await Promise.map(newList, (filePath) => {
     return new Promise((resolve, reject) => {
       shell.exec(
         `npx ${__dirname}/node_modules/.bin/babel ${filePath} --out-file ${filePath} --presets=@babel/preset-env`,
@@ -38,24 +41,26 @@ const preprocessJs = async tmpFolder => {
   return newList;
 };
 
-const readData = filePathList => {
-  let data = filePathList.map(filePath => {
+const readData = (filePathList) => {
+  let data = filePathList.map((filePath) => {
     const fileSourceCode = fs.readFileSync(filePath);
     const sandbox = {
-      exports: {}
+      exports: {},
     };
     const scope = vm.runInNewContext(fileSourceCode, sandbox);
     const sheet = sandbox.exports;
-    const translationKey = Object.keys(sheet).filter(key => key !== "filePath");
+    const translationKey = Object.keys(sheet).filter(
+      (key) => key !== "filePath"
+    );
     return {
       sheetName: sheet.filePath,
-      translations: sheet[translationKey[0]]
+      translations: sheet[translationKey[0]],
     };
   });
   return data;
 };
 
-const concatenate = async PATH => {
+const concatenate = async (PATH) => {
   const fileList = getTranslationsList(PATH);
   const tmpFolder = createTempFiles(fileList);
   const esFileList = await preprocessJs(tmpFolder);
@@ -66,5 +71,5 @@ const concatenate = async PATH => {
 };
 
 module.exports = {
-  concatenate
+  concatenate,
 };
